649df2f3d3ac2578c78ba8526a57c4a031f383e0
darrenlooby <darren.looby@googlemail.com>
2016-08-17 12:49:00 +0100

    Updated submodules

--- buildTree.js
+++ buildTree.js
@@ -5,44 +5,57 @@ function getProjects (folder) {
 
     var fileContents = fs.readdirSync(folder);
 
-    fileContents.map(function (fileName) {
+    var projects = fileContents.map(function (fileName) {
 
       var current = folder + '/' + fileName;
 
       var stats = fs.lstatSync(current);
 
       if (stats.isDirectory()) {
-        return current;
+        return {
+          name: fileName,
+          url: 'http://localhost:3000/' + fileName
+
+        };
+
       } else {
         return 'false';
       }
 
     });
 
-    return fileContents;
+    return projects;
 
 };
 
-function getResourceConfigs (name) {
+function getResourceConfigs (project, name) {
   var resource = cache.load(__dirname + '/source/resources/' + name + '.json');
   return resource;
 }
 
-function getImplementationConfigs (name) {
+function getImplementationConfigs (project, name) {
   var implementations = cache.load(__dirname + '/source/implementations/' + name + '.json');
   return implementations;
 }
 
+function getConfigs (project, name, type) {
+  var config = cache.load(__dirname + '/projects/' + project + '/source/' + type + 's/' + name + '.json');
+  console.log('project', project, name, type, config);
+  return config;
+}
 
-function buildConfig (name) {
+
+
+function buildConfig (project) {
   var fullConfig = {};
 
   for (var i = 0; i < load.length; i++) {
 
     var endpoint = load[i];
 
-    var resource = getResourceConfigs(endpoint);
-    var implementations = getImplementationConfigs(endpoint);
+    var resource = getConfigs(project, endpoint, 'implementation');
+    var implementations = getConfigs(project, endpoint, 'resource');
+
 
     var paths = Object.keys(resource);
 
@@ -63,21 +76,26 @@ function buildConfig (name) {
   return fullConfig;
 }
 
-
 var cache = require('./lib/cacheFiles.js');
 
 var obj = cache.load(__dirname + '/source/config.json');
 var load = obj.loadResources;
 
+// ##### Exmaple Server ##### //
 
 var express = require('express');
-
 var app = express();
 
 app.listen(3000);
 
-app.get('*', function (req, res, next) {
-  res.json(getProjects(__dirname + '/source'))
+app.get('/:project_name', function (req, res, next) {
 
-  // res.json(buildConfig());
+  res.json(buildConfig(req.params.project_name));
+
+})
+
+app.get('/', function (req, res, next) {
+
+  res.json(getProjects(__dirname + '/projects'))
+
 })
